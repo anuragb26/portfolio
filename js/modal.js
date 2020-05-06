a141d7d60fc52293a642147ab55b20fc2043dfb1
Anurag Bajaj <anuragb26@gmail.com>
2020-05-06 10:00:01 +0530

    Feat:Animate Modals

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,36 @@
 const portfolioContainer = document.querySelector(".portfolio-items");
 portfolioContainer.addEventListener("click", (event) => {
   event.preventDefault();
-  //   console.log("e", event);
   const modalToggle = event.target.closest(".portfolio-link");
-  console.log("modalToggle", modalToggle);
   if (!modalToggle) {
     return;
   }
   const modal = modalToggle.parentNode.nextElementSibling;
 
-  console.log("modal", modal);
   if (!modal) {
     return;
   }
-  modal.classList.add("is-open");
+
+  const modalOpen = () => {
+    modal.classList.add("is-open");
+    modal.style.animation = "modalIn 500ms forwards";
+    document.body.style.overflowY = "hidden";
+  };
+  const modalClose = () => {
+    modal.classList.remove("is-open");
+    modal.removeEventListener("animationend", modalClose);
+    document.body.style.overflowY = "scroll";
+  };
+  modalOpen();
   const closeButton = modal.querySelector(".modal-close");
   closeButton.addEventListener("click", (ev) => {
-    modal.classList.remove("is-open");
+    modal.style.animation = "modalOut 500ms forwards";
+    modal.addEventListener("animationend", modalClose);
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+      modal.style.animation = "modalOut 500ms forwards";
+      modal.addEventListener("animationend", modalClose);
+    }
   });
 });
